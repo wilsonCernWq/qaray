cddfe9021a225cb3f18bb4cd5cb26a0717d84906
WU Qi <qwu@WUs-MacBook-Air.local>
2017-08-28 23:20:50 -0600

    fix some compilation error on mac, not fully finished yet

--- cmake/configcxx.cmake
+++ cmake/configcxx.cmake
@@ -1,19 +1,14 @@
 #
 #--- Configures compiler for C++11
 #
+ADD_DEFINITIONS(-std=c++11)
 if(UNIX)
   if(NOT APPLE)
-    # Enable c++11 for GCC
     message(STATUS "Using GCC Compiler.")
-    set(CMAKE_C_COMPILER /opt/intel/bin/icc)
-    set(CMAKE_CXXXXXXXX_COMPILER /opt/intel/bin/icc)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
   else()
-    # Clang in OSX supports partially c++11 through extensions
     message(STATUS "Using CLANG Intel Compiler. (Untested)")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-c++11-extensions")
+    set(CMAKE_MACOSX_RPATH 1)
   endif()
 else()
-  # MSVC12 supports c++11 natively
   message(STATUS "Using MSVC Intel Compiler.")
 endif()
