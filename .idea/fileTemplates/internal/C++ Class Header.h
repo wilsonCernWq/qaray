65fdbaa1ac66dcddd3b21918fe743bb013dd799a
Qi WU <ubuntu@home.com>
2017-12-03 17:56:45 -0700

    Include CLion's configuration files

---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,14 @@
+#parse("C File Header.h")
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+#pragma once 
+
+${NAMESPACES_OPEN}
+
+class ${NAME} {
+
+};
+
+${NAMESPACES_CLOSE}
+
+#[[#endif]]# //${INCLUDE_GUARD}
