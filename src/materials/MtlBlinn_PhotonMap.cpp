758b6803e4aa25abc21abb564556d77850e6a8d6
Qi WU <wilson.over.cloud@gmail.com>
2017-12-12 00:49:04 -0700

    testing

--- src/materials/MtlBlinn_PhotonMap.cpp
+++ src/materials/MtlBlinn_PhotonMap.cpp
@@ -195,7 +195,7 @@ const
     } while (dot(sampleDir, Y) < 0);
     const Point3 L = normalize(sampleDir);
     const auto cosNL = MAX(0.f, dot(N, L));
-    BxDF = photonMap ? color : color * cosNL;
+    BxDF = photonMap ? color : color;
     const float y0 = SQRT(1.f / (reflectionGlossiness * reflectionGlossiness + 1.f));
     const float y1 = SQRT(1 - cosNL * cosNL);
     PDF = 0.5f / (1.f - MAX(y0, y1));
